28f4ddebed2759b5635360ae6a4cdb09194eb46c
Patrick42702 <patrick42702@yahoo.com>
2024-05-30 16:42:36 -0400

    beginning django implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is an astronmical on god {count}
+          count is an astronmical on god god {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
--- server/nerisfire/neris_app/serializers.py
+++ server/nerisfire/neris_app/serializers.py
@@ -1,10 +1,14 @@
-# serializers.py
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from django.contrib.auth.models import User
-# Serializers define the API representation.
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']

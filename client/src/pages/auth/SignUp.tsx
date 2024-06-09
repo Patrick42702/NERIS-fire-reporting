dba9815fa2ef5f9ac9bbf28d7332155b4baec5fc
jvariara <jvariara@gmail.com>
2024-06-09 18:15:36 -0400

    updated sign-up form to have 2 tabs & org phone number

--- client/src/pages/auth/SignUp.tsx
+++ client/src/pages/auth/SignUp.tsx
@@ -16,7 +16,7 @@ const SignUp = () => {
       <div className="container relative pt-20 flex flex-col items-center justifty-between lg:px-0">
         {/* Register stepper */}
         <div className="flex justify-between mb-4">
-          {/* {steps?.map((step, i) => (
+          {steps?.map((step, i) => (
             <div
               key={i}
               className={`step-item cursor-pointer ${
@@ -31,7 +31,7 @@ const SignUp = () => {
               </div>
               <p className="text-gray-500 font-semibold">{step}</p>
             </div>
-          ))} */}
+          ))}
         </div>
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
@@ -40,53 +40,78 @@ const SignUp = () => {
 
           {/* form */}
           <div className="grid gap-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="first-name">First name</Label>
-                <Input id="first-name" placeholder="Max" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="last-name">Last name</Label>
-                <Input id="last-name" placeholder="Robinson" required />
-              </div>
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="phone">Phone</Label>
-              <Input
-                id="phone"
-                type="tel"
-                placeholder="(xxx) xxx-xxxx"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="organization">Organization</Label>
-              <Input
-                id="organization"
-                type="text"
-                placeholder="Fire Department"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="confirm-password">Confirm Password</Label>
-              <Input id="confirm-password" type="password" />
-            </div>
-
-            <Button className="w-full">Create an account</Button>
+            {currentStep === 1 && (
+              <>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="first-name">First name</Label>
+                    <Input id="first-name" placeholder="Max" required />
+                  </div>
+                  <div className="grid gap-2">
+                    <Label htmlFor="last-name">Last name</Label>
+                    <Input id="last-name" placeholder="Robinson" required />
+                  </div>
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="phone">Phone</Label>
+                  <Input
+                    id="phone"
+                    type="tel"
+                    placeholder="(xxx) xxx-xxxx"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="confirm-password">Confirm Password</Label>
+                  <Input id="confirm-password" type="password" />
+                </div>
+              </>
+            )}
+            {currentStep === 2 && (
+              <>
+                <div className="grid gap-2">
+                  <Label htmlFor="organization">Organization</Label>
+                  <Input
+                    id="organization"
+                    type="text"
+                    placeholder="Fire Department"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="phone">Organization Phone</Label>
+                  <Input
+                    id="phone"
+                    type="tel"
+                    placeholder="(xxx) xxx-xxxx"
+                    required
+                  />
+                </div>
+              </>
+            )}
+            <Button
+              className="w-full"
+              onClick={() => {
+                currentStep === steps.length
+                  ? setComplete(true)
+                  : setCurrentStep((prev) => prev + 1);
+              }}
+            >
+              {currentStep === steps.length ? "Create an account" : "Next"}
+            </Button>
           </div>
           <Link
             to="/sign-in"

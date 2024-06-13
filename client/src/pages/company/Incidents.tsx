d88246c6985cf6cbcdb873c51f7e17b4e06a300f
jvariara <jvariara@gmail.com>
2024-06-12 20:27:43 -0400

    created basic layout for incidents tab

--- client/src/pages/company/Incidents.tsx
+++ client/src/pages/company/Incidents.tsx
@@ -1,8 +1,66 @@
+import { DataTable } from "@/components/ui/data-table";
 
 const Incidents = () => {
-  return (
-    <div>Incidents</div>
-  )
-}
+  const columns = [
+    {
+      accessorKey: "user",
+      header: "User",
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+    },
+    {
+      accessorKey: "started_at",
+      header: "Started At",
+    },
+    {
+      accessorKey: "ended_at",
+      header: "Ended At",
+    },
+  ];
 
-export default Incidents
+  const data = [
+    {
+      id: 1,
+      user: "Justin Variara",
+      date: "06/12/2024",
+      started_at: "07:18:05 PM",
+      ended_at: "07:30:44 PM",
+    },
+    {
+      id: 2,
+      user: "Chris Variara",
+      date: "04/12/2024",
+      started_at: "04:18:05 PM",
+      ended_at: "05:30:44 PM",
+    },
+    {
+      id: 3,
+      user: "Patrick Muller",
+      date: "05/12/2024",
+      started_at: "06:38:05 AM",
+      ended_at: "09:33:44 AM",
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="text-sm font-semibold md:text-lg text-primary-foreground tracking-wider uppercase">
+          Incidents
+        </h1>
+      </div>
+      <div className="w-full mx-auto mt-8 md:mt-20">
+        <DataTable
+          columns={columns}
+          data={data}
+          pageSize={10}
+          title="Incident Reports"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Incidents;

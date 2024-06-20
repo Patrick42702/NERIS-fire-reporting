4b7c758d13733c93064553518c2f38d6ae464c39
jvariara <jvariara@gmail.com>
2024-06-20 16:37:53 -0400

    added goals with radial progress on company dashboard

--- client/src/pages/company/CompanyDashboard.tsx
+++ client/src/pages/company/CompanyDashboard.tsx
@@ -1,78 +1,8 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { MoreHorizontal } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 const CompanyDashboard = () => {
   const { id } = useParams();
 
-  const columns = [
-    {
-      accessorKey: "user",
-      header: "User",
-    },
-    {
-      accessorKey: "email",
-      header: "Email",
-    },
-    {
-      accessorKey: "company",
-      header: "Company",
-    },
-    {
-      accessorKey: "action",
-      header: "Actions",
-      id: "actions",
-      cell: ({ row }: any) => {
-        console.log(row.original);
-
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Accept</DropdownMenuItem>
-              <DropdownMenuItem>Deny</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
-      },
-    },
-  ];
-
-  const data = [
-    {
-      id: 1,
-      user: "Justin Variara",
-      email: "jvariara@gmail.com",
-      company: "Fire Department 1",
-    },
-    {
-      id: 2,
-      user: "Chris Variara",
-      email: "cvariara@gmail.com",
-      company: "Fire Department 2",
-    },
-    {
-      id: 3,
-      user: "Patrick Muller",
-      email: "patrick42702@yahoo.com",
-      company: "Fire Department 3",
-    },
-  ];
   return (
     <>
       <div className="flex items-center">
@@ -84,6 +14,123 @@ const CompanyDashboard = () => {
         <h1 className="text-primary-foreground text-2xl md:text-3xl lg:text-5xl font-semibold">
           Hello Company Name
         </h1>
+        {/* grid */}
+        <div className="grid md:grid-cols-3 md:grid-rows-3 gap-4 w-full h-fit mt-4 lg:mt-12">
+          {/* Company Goals */}
+          <div className="border border-gray-200 col-span-3 lg:col-span-3 xl:col-span-2 sm:col-span-3 md:col-span-2 row-span-3 h-full rounded-md shadow-md w-full bg-card">
+            <div className="p-4 md:p-6 lg:h-full h-fit w-full flex">
+              <div className="flex flex-col w-full h-full gap-y-6">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-card-foreground">
+                  Company Goals
+                </h1>
+
+                <div className="flex justify-around items-center flex-col lg:flex-row sm:flex-row md:flex-col">
+                  <div className="flex flex-col justify-center items-center w-fit gap-y-4">
+                    <div
+                      className="radial-progress text-primary font-semibold text-2xl mx-auto md:mx-0 bg-secondary"
+                      // @ts-ignore
+                      style={{ "--value": 12, "--size": "12rem" }}
+                      role="progressbar"
+                    >
+                      12%
+                    </div>
+                    <h1 className="text-xl lg:text-2xl text-muted-foreground">
+                      <span className="text-primary font-bold">12%</span> total
+                      calls today
+                    </h1>
+                  </div>
+                  <div className="flex flex-col justify-center items-center w-fit gap-y-4">
+                    <div
+                      className="radial-progress text-primary font-semibold text-2xl mx-auto md:mx-0 bg-secondary"
+                      // @ts-ignore
+                      style={{ "--value": 90, "--size": "12rem" }}
+                      role="progressbar"
+                    >
+                      90%
+                    </div>
+                    <h1 className="text-xl lg:text-2xl text-muted-foreground">
+                      <span className="text-primary font-bold">90%</span> total
+                      calls YTD
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Personal Goals */}
+          <div className="border border-gray-200 col-span-3 md:col-span-1 row-span-1 h-full rounded-md shadow-md w-full bg-card">
+            <div className="p-4 md:p-6 lg:h-full h-fit w-full flex">
+              <div className="flex justify-between w-full gap-x-4 md:gap-y-4 lg:gap-y-4 md:flex-col xl:flex-row">
+                <div className="flex flex-col gap-y-2">
+                  <h2 className="text-muted-foreground uppercase text-sm tracking-tigh font-medium">
+                    Personal - Calls Today
+                  </h2>
+                  <h1 className="text-2xl md:text-3xl font-bold text-primary">
+                    2%
+                  </h1>
+                </div>
+
+                <div
+                  className="radial-progress text-primary font-semibold text-lg bg-secondary"
+                  // @ts-ignore
+                  style={{ "--value": 2, "--size": "4rem" }}
+                  role="progressbar"
+                >
+                  2%
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="border border-gray-200 col-span-3 md:col-span-1 row-span-1 h-full rounded-md shadow-md w-full bg-card">
+            <div className="p-4 md:p-6 lg:h-full h-fit w-full flex">
+              <div className="flex justify-between w-full gap-x-4 md:gap-y-4 lg:gap-y-4 md:flex-col xl:flex-row">
+                <div className="flex flex-col gap-y-2">
+                  <h2 className="text-muted-foreground uppercase text-sm tracking-tigh font-medium">
+                    Personal - Calls YTD
+                  </h2>
+                  <h1 className="text-2xl md:text-3xl font-bold text-primary">
+                    36%
+                  </h1>
+                </div>
+
+                <div
+                  className="radial-progress text-primary font-semibold text-lg bg-secondary"
+                  // @ts-ignore
+                  style={{ "--value": 36, "--size": "4rem" }}
+                  role="progressbar"
+                >
+                  36%
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="border border-gray-200 col-span-3 md:col-span-1 row-span-1 h-full rounded-md shadow-md w-full bg-card">
+            <div className="p-4 md:p-6 lg:h-full h-fit w-full flex">
+              <div className="flex justify-between w-full gap-x-4 md:gap-y-4 lg:gap-y-4 md:flex-col xl:flex-row">
+                <div className="flex flex-col gap-y-2">
+                  <h2 className="text-muted-foreground uppercase text-sm tracking-tigh font-medium">
+                    Personal - Calls Last Year
+                  </h2>
+                  <h1 className="text-2xl md:text-3xl font-bold text-primary">
+                    67%
+                  </h1>
+                </div>
+
+                <div
+                  className="radial-progress text-primary font-semibold text-lg bg-secondary"
+                  // @ts-ignore
+                  style={{ "--value": 67, "--size": "4rem" }}
+                  role="progressbar"
+                >
+                  67%
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- client/src/pages/company/CompanyLayout.tsx
+++ client/src/pages/company/CompanyLayout.tsx
@@ -9,7 +9,7 @@ const CompanyLayout = () => {
       <Header page="company" id={id} />
       <main className="flex flex-1 flex-col gap-4 lg:gap-6 bg-[#F9F9F9] overflow-auto ml-0 lg:ml-[300px]">
         <div className="h-80 bg-primary bg-gradient-to-r from-primary to-red-500 w-full" />
-        <div className="p-4 lg:p-8 -mt-[336px] lg:-mt-[344px]">
+        <div className="p-4 lg:p-8 -mt-[336px] lg:-mt-[344px] h-full">
           <Outlet />
         </div>
       </main>

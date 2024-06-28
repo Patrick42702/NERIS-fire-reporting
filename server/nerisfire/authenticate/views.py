aad38e7bb1a97ee5708e874b01e40ee8354d6e86
Christopher <cvariara@gmail.com>
2024-06-28 19:42:11 -0400

    updated member view, serializers, and urls for auth and jwt

--- server/nerisfire/authenticate/views.py
+++ server/nerisfire/authenticate/views.py
@@ -1,16 +1,17 @@
 from base.models import Member
-from rest_framework import serializers
+#from rest_framework import serializers
 from rest_framework import generics
+from .seralizers import MemberSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 import logging
 
 logger = logging.getLogger('authenticate')  # Use the same logger name as defined in settings
 
-#Create views here
+# Create views here
 
 class CreateMemberView(generics.CreateAPIView):
     queryset = Member.objects.all()
-    serializer_class = Member
+    serializer_class = MemberSerializer
     permission_classes = [AllowAny]
 
 # class RegisterUserView(APIView):

937fc470648f6eb019927b62f7e90332fab01a6d
Patrick42702 <patrick42702@yahoo.com>
2024-06-30 13:51:47 -0400

    Reformatted login and register views and urls

--- server/nerisfire/authenticate/urls.py
+++ server/nerisfire/authenticate/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import LoginView, LogoutView, RegisterUserView
+from .views import CreateMemberView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterUserView.as_view(), name='register')
+    path('member/register', CreateMemberView.as_view(), name="register"),
+    path('token', TokenObtainPairView.as_view(), name="get_token"),
+    path('token/refresh', TokenRefreshView.as_view(), name="refresh"),
 ]

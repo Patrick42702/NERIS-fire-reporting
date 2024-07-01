fb5d8835f911d533a28b56f82a0ea8fc9ef4f2d0
Patrick Muller <patrick.muller.1@stonybrook.edu>
2024-07-01 11:08:05 -0400

    Fixed some settings and .gitignore
    Fixed gitignore to ignore log files from now on
    removed unnecessary backends
    removed dead code

--- server/nerisfire/authenticate/seralizers.py
+++ server/nerisfire/authenticate/seralizers.py
@@ -13,20 +13,3 @@ class MemberSerializer(serializers.ModelSerializer):
         )
         return member
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = Member
-        fields = ['email', 'password', 'first_name', 'last_name', 'phone', 'username']
-
-    def create(self, validated_data):
-        user = Member.objects.create_user(
-            email=validated_data.get('email'),
-            password=validated_data.get('password'),
-            first_name=validated_data.get('fname'),
-            last_name=validated_data.get('lname'),
-            phone=validated_data.get('phone'),
-            username=email,
-        )
-        return user

64f7f21c404be23fcc11f4174fecf062173d4f0c
Christopher <cvariara@gmail.com>
2024-07-04 13:38:11 -0400

    Added GET req views and urls for organization

--- server/nerisfire/api/urls.py
+++ server/nerisfire/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import CreateOrganizationView
+from .views import CreateOrganizationView, ListOrganizationView, RetrieveOrganizationView
 
 urlpatterns = [
-    path('org/register', CreateOrganizationView.as_view(), name="register org")
+    path('org/register', CreateOrganizationView.as_view(), name="register_org"),
+    path('org/list', ListOrganizationView.as_view(), name="list_orgs"),
+    path('org/<uuid:pk>', RetrieveOrganizationView.as_view(), name="retrieve_org")
 ]
--- server/nerisfire/api/views.py
+++ server/nerisfire/api/views.py
@@ -3,13 +3,20 @@ from rest_framework import permissions, viewsets, generics
 from base.models import Organization
 from .serializers import OrganizationSerializer
 
+# POST request -> creates new org
 class CreateOrganizationView(generics.CreateAPIView):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+# GET request -> list of all orgs
+class ListOrganizationView(generics.ListAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    permission_classes = [permissions.IsAuthenticated]    
 
-
-
-
-
+# GET request -> retriev es a single org by its ID
+class RetrieveOrganizationView(generics.RetrieveAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    permission_classes = [permissions.IsAuthenticated]

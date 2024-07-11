b6da7f0eff36c57845af79727ae97a857bfa6a20
Christopher <cvariara@gmail.com>
2024-07-10 20:33:24 -0400

    Changed view for getting/updating/deleting an org into a single view

--- server/nerisfire/api/views.py
+++ server/nerisfire/api/views.py
@@ -30,23 +30,29 @@ class ListOrganizationView(generics.ListAPIView):
         
         return queryset
 
-# GET request -> retrieves a single org by its ID
-class RetrieveOrganizationView(generics.RetrieveAPIView):
+# GETs, PUTs, and/or DELETEs a single org by its ID
+class RetrieveUpdateDestroyOrganizationView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+# # GET request -> retrieves a single org by its ID
+# class RetrieveOrganizationView(generics.RetrieveAPIView):
+#     queryset = Organization.objects.all()
+#     serializer_class = OrganizationSerializer
+#     permission_classes = [permissions.IsAuthenticated]
     
-# PUT request -> updates a single org by its ID
-class UpdateOrganizationView(generics.UpdateAPIView):
-    queryset = Organization.objects.all()
-    serializer_class = OrganizationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# # PUT request -> updates a single org by its ID
+# class UpdateOrganizationView(generics.UpdateAPIView):
+#     queryset = Organization.objects.all()
+#     serializer_class = OrganizationSerializer
+#     permission_classes = [permissions.IsAuthenticated]
    
-# DELETE request -> deletes a single org by its ID 
-class DeleteOrganizationView(generics.DestroyAPIView):
-    queryset = Organization.objects.all()
-    serializer_class = OrganizationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# # DELETE request -> deletes a single org by its ID 
+# class DeleteOrganizationView(generics.DestroyAPIView):
+#     queryset = Organization.objects.all()
+#     serializer_class = OrganizationSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
 class StatusRangesView(generics.ListAPIView):
     queryset = StatusRanges.objects.all()
